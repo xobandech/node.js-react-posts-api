0324534309d942ef6a2b63a680cb67ed65b341fd
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-07 18:25:22 +0200

    Create createPost method

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,12 +1,34 @@
-exports.getPosts = (req, res) => {
-    res.json({
-        posts: [
-            {
-                title: "First post"
-            },
-            {
-                title: "second post "
-            }
-        ]
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.getPosts = async (req, res) => {
+ await prisma.post
+    .findMany()
+    .then((posts) => {
+      res.send(posts);
     })
-}
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Failed to fetch posts.");
+    });
+    res.end
+};
+
+exports.getPostById = async (req, res) => {
+  const { id } = req.params;
+  await prisma.post.findUnique({ where: { id: +id } }).then((post) => res.send(post));
+  res.end
+};
+
+exports.createPost = async (req, res) => {
+  const { title, message } = req.body;
+
+  await prisma.post
+    .create({ data: { title, message } })
+    .then((createdPost) => res.status(200).send(createdPost))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Failed to create post.");
+    });
+    res.end
+};
