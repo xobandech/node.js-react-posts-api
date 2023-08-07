b487b1af9fe2ef5a9a7f5ae4833e226f49da07bc
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-07 18:25:10 +0200

    Cleanup

--- app/app.js
+++ app/app.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+  
+const postRoutes = require("../src/routers/postRouter");
 const app = express();
-const { PrismaClient } = require("@prisma/client")
-const postRoutes = require('../src/routers/postRouter');
-const prisma = new PrismaClient()
-const myMiddleware = (req, res, next) => {
-  console.log("myMiddleware applied");
-  next()
-};
-app.use('/', postRoutes)
+
+app.use("/", postRoutes);
 app.use(morgan("dev"));
-app.use(myMiddleware);
 
 app.listen(3000);
-
-prisma.post.create({data: {
-  message: "Hi", title:"Welcome"
-}
-}).then(() => console.log("succes"))
