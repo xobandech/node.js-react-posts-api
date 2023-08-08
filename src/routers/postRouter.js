cb316372ba6876594a6e777b495e99a7b5c0ed8a
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-08 17:48:52 +0200

    Add backslash in the statr of every route

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -2,11 +2,12 @@ const postController = require("../controllers/postController");
 const express = require("express");
 const postRouter = express.Router();
 
-postRouter.get("posts/", postController.getPosts);
+postRouter.get("/posts", postController.getPosts);
 
-postRouter.get("posts/:id", postController.getPostById)
+postRouter.get("/posts/:id", postController.getPostById);
 
-postRouter.post("posts/create", postController.createPost);
+postRouter.post("/posts/create", postController.createPost);
 
-postRouter.delete("posts/delete/:id", postController.deletePostById)
+postRouter.delete("/posts/delete/:id", postController.deletePostById);
+
 module.exports = postRouter;
