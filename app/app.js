caa7b965fbab80d77970080b7a75bbe3a4a478eb
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-08 17:49:13 +0200

    Clean up

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-  
+
 const postRoutes = require("../src/routers/postRouter");
 const app = express();
 
-app.use("/", postRoutes);
+app.use(express.json());
 app.use(morgan("dev"));
+app.use("/", postRoutes);
 
 app.listen(3000);
