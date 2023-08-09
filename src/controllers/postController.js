52b1a106a9110d86ad40679afbb3701323f633bf
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-09 11:37:50 +0200

    Create parchPostById func

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -24,7 +24,7 @@ exports.getPostById = async (req, res) => {
 
 exports.deletePostById = async (req, res) => {
   const { id } = req.params; 
-  
+
   try {
     const deletedPost = await prisma.post.delete({ where: { id: +id } });
     res
@@ -51,4 +51,37 @@ exports.createPost = async (req, res) => {
       res.status(500).send("Failed to create post.");
     });
   res.end;
+};
+
+exports.patchPostById = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, message } = req.body;
+
+  try {
+      let updatedPost;
+
+      if (title && message) {
+          updatedPost = await prisma.post.update({
+              where: { id: id },
+              data: { title, message },
+          });
+      } else if (title) {
+          updatedPost = await prisma.post.update({
+              where: { id: id },
+              data: { title },
+          });
+      } else if (message) {
+          updatedPost = await prisma.post.update({
+              where: { id: id },
+              data: { message },
+          });
+      } else {
+          return res.status(400).json({ success: false, error: "No fields to update." });
+      }
+
+      res.status(200).json({ success: true, data: updatedPost });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: "Failed to update post." });
+  }
 };
