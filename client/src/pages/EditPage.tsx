baf57785756958260bff22a3e5884f1c123ea9fd
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-09 22:43:59 +0200

    Create EditPage component

---
+++ client/src/pages/EditPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import FindPostComponent from "../components/FindPostComponent"
+
+const EditPage = () => {
+    return <FindPostComponent></FindPostComponent>
+}
+
+export default EditPage
