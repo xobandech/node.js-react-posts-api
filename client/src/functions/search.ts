c2e4b6c76a5085596c766432914a512051e825ec
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-10 21:07:06 +0200

    Create editPostById query

--- client/src/functions/search.ts
+++ client/src/functions/search.ts
@@ -4,7 +4,11 @@ export const searchPostsById = async (id: string) => {
   return await fetch(`/posts/${id}`).then((res) => res.json());
 };
 
-export const editPostById = async ({ id, title, message }: Post) => {};
+export const editPostById = async ({ id, title, message }: Post) => {    
+    return await fetch(`/posts/edit?id=${id}&title=${title}&message=${message}`, {
+        method: "PATCH"
+    })
+};
 
 export const searchPosts = async ({ id, title, message }: Post) => {
   return await fetch(
