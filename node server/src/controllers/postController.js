a628d8ac8db6d9691eea9ce54e91fc8f53adac5d
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-10 12:45:54 +0200

    Create case-insensitive search

--- node server/src/controllers/postController.js
+++ node server/src/controllers/postController.js
@@ -23,17 +23,15 @@ exports.getPostById = async (req, res) => {
 };
 
 exports.deletePostById = async (req, res) => {
-  const { id } = req.params; 
+  const { id } = req.params;
 
   try {
     const deletedPost = await prisma.post.delete({ where: { id: +id } });
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Post deleted successfully",
-        data: deletedPost,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+      data: deletedPost,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: "Failed to delete post." });
@@ -58,30 +56,57 @@ exports.patchPostById = async (req, res) => {
   const { title, message } = req.body;
 
   try {
-      let updatedPost;
+    let updatedPost;
 
-      if (title && message) {
-          updatedPost = await prisma.post.update({
-              where: { id: id },
-              data: { title, message },
-          });
-      } else if (title) {
-          updatedPost = await prisma.post.update({
-              where: { id: id },
-              data: { title },
-          });
-      } else if (message) {
-          updatedPost = await prisma.post.update({
-              where: { id: id },
-              data: { message },
-          });
-      } else {
-          return res.status(400).json({ success: false, error: "No fields to update." });
-      }
+    if (title && message) {
+      updatedPost = await prisma.post.update({
+        where: { id: id },
+        data: { title, message },
+      });
+    } else if (title) {
+      updatedPost = await prisma.post.update({
+        where: { id: id },
+        data: { title },
+      });
+    } else if (message) {
+      updatedPost = await prisma.post.update({
+        where: { id: id },
+        data: { message },
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, error: "No fields to update." });
+    }
 
-      res.status(200).json({ success: true, data: updatedPost });
+    res.status(200).json({ success: true, data: updatedPost });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, error: "Failed to update post." });
+    console.error(error);
+    res.status(500).json({ success: false, error: "Failed to update post." });
+  }
+};
+
+exports.searchPosts = async (req, res) => {
+  try {
+    const { id, message, title } = req.query;
+
+    const posts = await prisma.post.findMany({
+      where: {
+        AND: [
+          id ? { id: parseInt(id) } : undefined,
+          message
+            ? { message: { contains: message, mode: "insensitive" } }
+            : undefined,
+          title
+            ? { title: { contains: title, mode: "insensitive" } }
+            : undefined,
+        ],
+      },
+    });
+
+    res.json(posts);
+  } catch (error) {
+    console.error("Error searching posts:", error);
+    res.status(500).json({ error: "An error occurred while searching posts." });
   }
 };
