f9e0aee2cb5a1255a3fef553d92d4a8367abcad0
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-10 19:10:02 +0200

    Split FindPostComponent in two components, because i forgot that i write only FindPostComponent hehe

--- client/src/components/FindPostComponent.tsx
+++ client/src/components/FindPostComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Post } from "../types/types";
-
+import { searchPostsById, editPostById } from "../functions/search";
 const FindPostComponent = () => {
   const [posts, setPosts] = useState<Post[]>();
   const [searchFormFields, setSearchFormFields] = useState({
@@ -8,12 +8,7 @@ const FindPostComponent = () => {
     title: "",
     message: "",
   });
-  const [editPostFields, setEditPostFields] = useState({
-    id: "",
-    title: "",
-    message: "",
-  });
-  const [editPostId, setEditPostId] = useState("")
+ 
   const searchPosts = async () => {
     await fetch(
       `/posts/search?id=${searchFormFields.id}&title=${searchFormFields.title}&message=${searchFormFields.message}`
@@ -21,15 +16,9 @@ const FindPostComponent = () => {
       .then((res) => res.json())
       .then((data) => setPosts(data));
   };
-  const searchPostsById = async (id: string) => {
-    return await fetch(
-        `/posts/${id}`
-    ).then(res => res.json())
-  }
 
-  const editPostById = async ({id, title, message}: Post) => {
 
-  }
+
   return (
     <div className="m-2">
       <div>
@@ -69,49 +58,6 @@ const FindPostComponent = () => {
               }
             />
           </form>
-          <div className="flex w-1/3 flex-col [&>label]:leading-[5px]">
-            <div className="flex ">
-              <div>
-                <label htmlFor="postToEdit">ID of post to Edit</label>
-                <input type="text" onChange={(e) => setEditPostId(e.target.value)} className="outline outline-1 my-2" />
-              </div>
-              <button className="h-1/2 bg-gray-300 rounded-md px-2 outline outline-1" onClick={() => searchPostsById(editPostId).then(data => setEditPostFields(data))}>
-                Search
-              </button>
-            </div>
-
-            <label htmlFor="id">Id</label>
-            <input
-              type="text"
-              className="outline outline-1 my-2"
-              disabled
-              value={editPostFields.id}
-            />
-            <label htmlFor="title">Title</label>
-            <textarea
-              rows={2}
-              className="outline outline-1 my-2"
-              value={editPostFields.title}
-              onChange={(e) =>
-                setEditPostFields({ ...editPostFields, title: e.target.value })
-              }
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
-              rows={3}
-              className="outline outline-1 my-2"
-              value={editPostFields.message}
-              onChange={(e) =>
-                setEditPostFields({
-                  ...editPostFields,
-                  message: e.target.value,
-                })
-              }
-            />
-            <button className="max-h-[30px] h-[50%] bg-gray-300 rounded-md px-2 outline outline-1 w-1/2 mx-auto">
-                Edit
-            </button>
-          </div>
         </div>
         <button onClick={searchPosts}>Search for post</button>
       </div>
--- client/src/pages/EditPage.tsx
+++ client/src/pages/EditPage.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FindPostComponent from "../components/FindPostComponent"
+import { searchPostsById } from '../functions/search';
 
 const EditPage = () => {
-    return <FindPostComponent></FindPostComponent>
+    const [editPostFields, setEditPostFields] = useState({
+        id: "",
+        title: "",
+        message: "",
+      });
+      const [editPostId, setEditPostId] = useState("")
+
+    return <>
+        <FindPostComponent />
+        <div className="flex w-1/3 flex-col [&>label]:leading-[5px]">
+            <div className="flex ">
+              <div>
+                <label htmlFor="postToEdit">ID of post to Edit</label>
+                <input type="text" onChange={(e) => setEditPostId(e.target.value)} className="outline outline-1 my-2" />
+              </div>
+              <button className="h-1/2 bg-gray-300 rounded-md px-2 outline outline-1" onClick={() => searchPostsById(editPostId).then(data => setEditPostFields(data))}>
+                Search
+              </button>
+            </div>
+
+            <label htmlFor="id">Id</label>
+            <input
+              type="text"
+              className="outline outline-1 my-2"
+              disabled
+              value={editPostFields.id}
+            />
+            <label htmlFor="title">Title</label>
+            <textarea
+              rows={2}
+              className="outline outline-1 my-2"
+              value={editPostFields.title}
+              onChange={(e) =>
+                setEditPostFields({ ...editPostFields, title: e.target.value })
+              }
+            />
+            <label htmlFor="message">Message</label>
+            <textarea
+              rows={3}
+              className="outline outline-1 my-2"
+              value={editPostFields.message}
+              onChange={(e) =>
+                setEditPostFields({
+                  ...editPostFields,
+                  message: e.target.value,
+                })
+              }
+            />
+            <button className="max-h-[30px] h-[50%] bg-gray-300 rounded-md px-2 outline outline-1 w-1/2 mx-auto">
+                Edit
+            </button>
+          </div>
+    </>
+
 }
 
 export default EditPage
