4981b650afeb7816db0800dd5021aba5c36d5c42
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-10 21:07:53 +0200

    Place edit route on top of the routes

--- node server/src/routers/postRouter.js
+++ node server/src/routers/postRouter.js
@@ -2,16 +2,18 @@ const postController = require("../controllers/postController");
 const express = require("express");
 const postRouter = express.Router();
 
+postRouter.patch("/posts/edit/", postController.patchPostById);
+
 postRouter.get("/posts", postController.getPosts);
+
 
 postRouter.get('/posts/search', postController.searchPosts);
 
-postRouter.get("/posts/:id", postController.getPostById);
+postRouter.get("/posts/:id", postController.getPostById);       
 
 postRouter.post("/posts", postController.createPost);
 
 postRouter.delete("/posts/:id", postController.deletePostById);
 
-postRouter.patch("/posts/:id", postController.patchPostById);
 
 module.exports = postRouter;
