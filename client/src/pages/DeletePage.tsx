da5a7e7113573502b41473168dd9379b16da0b86
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 13:16:37 +0200

    Create DeletePage

---
+++ client/src/pages/DeletePage.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import FindPostComponent from "../components/FindPostComponent"
+
+const DeletePage = () => {
+    return <div>
+        <FindPostComponent />
+    </div>
+}
+export default DeletePage
