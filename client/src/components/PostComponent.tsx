e2b2b493907e1237c018fdb9ea6c8d1af3d8f13c
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 13:50:59 +0200

    Add id as a key

--- client/src/components/PostComponent.tsx
+++ client/src/components/PostComponent.tsx
@@ -2,7 +2,7 @@ import { Post } from "../types/types";
 
 const PostComponent = ({ post }: { post: Post }) => {
   return (
-    <div id={post.id?.toLocaleString()} className="outline outline-1 w-1/3 m-2 min-w-[350px]">
+    <div key={post.id} id={post.id?.toLocaleString()} className="outline outline-1 w-1/3 m-2 min-w-[350px]">
       <h3 className="font-bold">{post.title}</h3>
       <p>{post.message}</p>
       <p className="text-sm text-gray-400">{post.id}</p>
