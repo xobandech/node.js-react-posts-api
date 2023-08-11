40132cdaa9ebc274df1ca4b77942332cefa57530
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 13:43:55 +0200

    Create DeletePage

--- client/src/pages/DeletePage.tsx
+++ client/src/pages/DeletePage.tsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
-import FindPostComponent from "../components/FindPostComponent"
+import { useState } from "react";
+import FindPostComponent from "../components/FindPostComponent";
+import { deletePost } from "../functions/postFunctions";
 
 const DeletePage = () => {
-    return <div>
-        <FindPostComponent />
+  const [postId, setPostId] = useState("");
+  return (
+    <div>
+      <FindPostComponent />
+      <div className="flex flex-col items-left max-w-[200px]">
+        <label className="">Id of post to Delete</label>
+        <input
+          className="outline outline-1 my-2"
+          onChange={(e) => setPostId(e.target.value)}
+          type="number"
+        />
+        <button
+          className="bg-gray-300 rounded-md my-auto max-w-[70%] px-2 outline outline-1"
+          disabled={postId.length <= 0}
+          onClick={() => deletePost(postId)}
+        >
+          Delete
+        </button>
+      </div>
     </div>
-}
-export default DeletePage
+  );
+};
+export default DeletePage;
