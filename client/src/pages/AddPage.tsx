f8bff881043d340aa2872d120bab4c50be3ed399
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 12:06:43 +0200

    Create AddPage component

---
+++ client/src/pages/AddPage.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import FindPostComponent from "../components/FindPostComponent"
+
+const AddPage = () => {
+    return <div className='flex'>
+        <FindPostComponent />
+        <div>
+            
+        </div>
+    </div>
+}
+
+export default AddPage
