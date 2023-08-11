75e03598e0bf993103b38c871e0b6eb81c031f3a
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 12:48:55 +0200

    Add createPost function & add response message

--- client/src/functions/postFunctions.ts
+++ client/src/functions/postFunctions.ts
@@ -4,14 +4,53 @@ export const searchPostsById = async (id: string) => {
   return await fetch(`/posts/${id}`).then((res) => res.json());
 };
 
-export const editPostById = async ({ id, title, message }: Post) => {    
-    return await fetch(`/posts/edit?id=${id}&title=${title}&message=${message}`, {
-        method: "PATCH"
-    })
+export const editPostById = async ({ id, title, message }: Post) => {
+  try {
+    const response = await fetch(`/posts/edit?id=${id}&title=${title}&message=${message}`, {
+      method: "PATCH"
+    });
+
+    if (response.ok) {
+      return response.json(); 
+    } else {
+      throw new Error("Edit post request failed");
+    }
+  } catch (error) {
+    console.error("An error occurred while editing the post:", error);
+    throw error;
+  }
+};
+
+export const createPost = async ({ title, message }: Post) => {
+  try {
+    const response = await fetch(`/posts?title=${title}&message=${message}`, {
+      method: "POST"
+    });
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Create post request failed");
+    }
+  } catch (error) {
+    console.error("An error occurred while creating the post:", error);
+    throw error;
+  }
 };
 
 export const searchPosts = async ({ id, title, message }: Post) => {
-  return await fetch(
-    `/posts/search?id=${id ? id : ""}&title=${title}&message=${message}`
-  ).then((res) => res.json());
+  try {
+    const response = await fetch(
+      `/posts/search?id=${id ? id : ""}&title=${title}&message=${message}`
+    );
+
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Search posts request failed");
+    }
+  } catch (error) {
+    console.error("An error occurred while searching for posts:", error);
+    throw error;
+  }
 };
