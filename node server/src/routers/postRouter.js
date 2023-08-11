fcf98555680255dae6fc88012feecd4737571853
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-11 13:13:36 +0200

    Cleand up

--- node server/src/routers/postRouter.js
+++ node server/src/routers/postRouter.js
@@ -2,18 +2,16 @@ const postController = require("../controllers/postController");
 const express = require("express");
 const postRouter = express.Router();
 
+postRouter.post("/posts", postController.createPost);
+
 postRouter.patch("/posts/edit/", postController.patchPostById);
 
 postRouter.get("/posts", postController.getPosts);
-
 
 postRouter.get('/posts/search', postController.searchPosts);
 
 postRouter.get("/posts/:id", postController.getPostById);       
 
-postRouter.post("/posts", postController.createPost);
-
 postRouter.delete("/posts/:id", postController.deletePostById);
-
 
 module.exports = postRouter;
